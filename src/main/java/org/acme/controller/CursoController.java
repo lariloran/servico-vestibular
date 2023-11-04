30de1930d955bcdc4f30f4bd1c5f6cb939e247c4
Rafael Rodrigues <lariloran2@gmail.com>
2023-11-04 16:38:22 -0300

    Criando CRUD de curso

--- src/main/java/org/acme/controller/CursoController.java
+++ src/main/java/org/acme/controller/CursoController.java
@@ -1,16 +1,77 @@
 package org.acme.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.acme.domain.Curso;
+import org.acme.domain.ModalidadeEnsino;
+import org.acme.service.CursoService;
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/api/v1/curso")
 public class CursoController {
 
+    @Inject
+    CursoService cursoService;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Curso> ObterTodosCurso() {
+        List<Curso> cursos = new ArrayList<>();
+
+        try {
+            cursos = cursoService.ObterTodosCursosService();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cursos;
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Curso ObterCursoPorId(@PathParam("id") Long id) {
+        Curso curso = new Curso();
+
+        try {
+            curso = cursoService.ObterCursoPorIdService(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return curso;
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void AdicionarCurso(Curso curso){
+            cursoService.AdicionarCursoService(curso);
+    }
+
+    @PUT
+    @Transactional
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void AtualizarCurso(@PathParam("id") Long id, Curso curso){
+            cursoService.AtualizarCursoService(id, curso);
+    }
+
+    @DELETE
+    @Transactional
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void RemoverCurso(@PathParam("id") Long id){
+            cursoService.RemoverCursoService(id);
     }
 }
--- src/main/java/org/acme/service/CursoService.java
+++ src/main/java/org/acme/service/CursoService.java
@@ -1,11 +1,43 @@
 package org.acme.service;
 
-import org.acme.repository.CursoRepository;
+import java.util.List;
 
+import org.acme.domain.Curso;
+import org.acme.domain.ModalidadeEnsino;
+import org.acme.repository.CursoRepository;
+import org.acme.repository.ModalidadeEnsinoRepository;
+
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+@ApplicationScoped
 public class CursoService {
 
    @Inject
    CursoRepository cursoRepository;
+    
+    public List<Curso> ObterTodosCursosService(){
+        return cursoRepository.findAll().list();
+    }
+
+    public Curso ObterCursoPorIdService(Long id){
+        return cursoRepository.findById(id);
+    }
+
+    public void AdicionarCursoService(Curso curso){
+        cursoRepository.persist(curso);
+    }
+
+    public void AtualizarCursoService(Long id,Curso curso){
+        Curso obj = cursoRepository.findById(id);
+        if(obj != null){
+            obj.setNome(curso.getNome());
+            obj.setDescricao(curso.getDescricao());
+            cursoRepository.persist(obj);
+        }
+    }
+
+    public void RemoverCursoService(Long id){
+        cursoRepository.deleteById(id);
+    }
 }
