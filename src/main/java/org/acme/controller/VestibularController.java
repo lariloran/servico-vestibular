f21d33a0aea2c18b824ef0499e4253bb30a3a4f0
Rafael Rodrigues <lariloran2@gmail.com>
2023-11-04 17:15:47 -0300

    Criando CRUD de Vestibular

--- src/main/java/org/acme/controller/VestibularController.java
+++ src/main/java/org/acme/controller/VestibularController.java
@@ -1,16 +1,76 @@
 package org.acme.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.acme.domain.Vestibular;
+import org.acme.service.VestibularService;
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/api/v1/vestibular")
 public class VestibularController {
 
+    @Inject
+    VestibularService vestibularService;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Vestibular> ObterTodosVestibulares() {
+        List<Vestibular> vestibulares = new ArrayList<>();
+
+        try {
+            vestibulares = vestibularService.ObterTodosVestibularesService();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return vestibulares;
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Vestibular ObterVestibularPorId(@PathParam("id") Long id) {
+        Vestibular vestibular = new Vestibular();
+
+        try {
+            vestibular = vestibularService.ObterVestibularPorIdService(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return vestibular;
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void AdicionarVestibular(Vestibular vestibular){
+            vestibularService.AdicionarVestibularService(vestibular);
+    }
+
+    @PUT
+    @Transactional
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void AtualizarVestibular(@PathParam("id") Long id, Vestibular vestibular){
+            vestibularService.AtualizarVestibularService(id, vestibular);
+    }
+
+    @DELETE
+    @Transactional
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void RemoverVestibular(@PathParam("id") Long id){
+            vestibularService.RemoverVestibularService(id);
     }
 }
--- src/main/java/org/acme/service/VestibularService.java
+++ src/main/java/org/acme/service/VestibularService.java
@@ -1,5 +1,8 @@
 package org.acme.service;
 
+import java.util.List;
+
+import org.acme.domain.Vestibular;
 import org.acme.repository.VestibularRepository;
 
 import jakarta.enterprise.context.ApplicationScoped;
@@ -10,4 +13,36 @@ public class VestibularService {
 
    @Inject
    VestibularRepository vestibularRepository;
+
+   public List<Vestibular> ObterTodosVestibularesService() {
+      return vestibularRepository.findAll().list();
+   }
+
+   public Vestibular ObterVestibularPorIdService(Long id) {
+      return vestibularRepository.findById(id);
+   }
+
+   public void AdicionarVestibularService(Vestibular vestibular) {
+      vestibularRepository.persist(vestibular);
+   }
+
+   public void AtualizarVestibularService(Long id,Vestibular vestibular){
+        Vestibular obj = vestibularRepository.findById(id);
+        if(obj != null){
+            obj.setNome(vestibular.getNome());
+            obj.setSigla(vestibular.getSigla());
+            obj.setDataInicio(vestibular.getDataInicio());
+            obj.setDataFim(vestibular.getDataFim());
+            obj.setNumeroEdital(vestibular.getNumeroEdital());
+            obj.setSemestre(vestibular.getSemestre());
+            obj.setAno(vestibular.getAno());
+            obj.setDataCadastro(vestibular.getDataCadastro());
+            obj.setAtivo(false);
+            vestibularRepository.persist(obj);
+        }
+    }
+
+   public void RemoverVestibularService(Long id) {
+      vestibularRepository.deleteById(id);
+   }
 }
