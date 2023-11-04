ca3db5c1d13ef0e57784e8568086779f6afbb165
Rafael Rodrigues <lariloran2@gmail.com>
2023-11-04 16:47:27 -0300

    Criando CRUD de Forma de Ingresso

--- src/main/java/org/acme/controller/FormaIngressoController.java
+++ src/main/java/org/acme/controller/FormaIngressoController.java
@@ -1,16 +1,78 @@
 package org.acme.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.acme.domain.Curso;
+import org.acme.domain.FormaIngresso;
+import org.acme.service.CursoService;
+import org.acme.service.FormaIngressoService;
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/api/v1/forma-ingresso")
 public class FormaIngressoController {
 
+    @Inject
+    FormaIngressoService formaIngressoService;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<FormaIngresso> ObterTodosFormasIngresso() {
+        List<FormaIngresso> formasIngresso = new ArrayList<>();
+
+        try {
+            formasIngresso = formaIngressoService.ObterTodasFormasIngressoService();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return formasIngresso;
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public FormaIngresso ObterFormaIngressoPorId(@PathParam("id") Long id) {
+        FormaIngresso formaIngresso = new FormaIngresso();
+
+        try {
+            formaIngresso = formaIngressoService.ObterFormaIngressoPorIdService(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return formaIngresso;
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void AdicionarFormaIngresso(FormaIngresso formaIngresso) {
+        formaIngressoService.AdicionarFormaIngressoService(formaIngresso);
+    }
+
+    @PUT
+    @Transactional
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void AtualizarFormaIngresso(@PathParam("id") Long id, FormaIngresso formaIngresso) {
+        formaIngressoService.AtualizarFormaIngressoService(id, formaIngresso);
+    }
+
+    @DELETE
+    @Transactional
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void RemoverFormaIngresso(@PathParam("id") Long id) {
+        formaIngressoService.RemoverFormaIngressoService(id);
     }
 }
--- src/main/java/org/acme/service/FormaIngressoService.java
+++ src/main/java/org/acme/service/FormaIngressoService.java
@@ -1,15 +1,42 @@
 package org.acme.service;
 
+import java.util.List;
+
+import org.acme.domain.Curso;
+import org.acme.domain.FormaIngresso;
+import org.acme.repository.CursoRepository;
 import org.acme.repository.FormaIngressoRepository;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+@ApplicationScoped
 public class FormaIngressoService {
 
     @Inject
     FormaIngressoRepository formaIngressoRepository;
+    
+    public List<FormaIngresso> ObterTodasFormasIngressoService(){
+        return formaIngressoRepository.findAll().list();
+    }
 
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public FormaIngresso ObterFormaIngressoPorIdService(Long id){
+        return formaIngressoRepository.findById(id);
+    }
+
+    public void AdicionarFormaIngressoService(FormaIngresso formaIngresso){
+        formaIngressoRepository.persist(formaIngresso);
+    }
+
+    public void AtualizarFormaIngressoService(Long id,FormaIngresso formaIngresso){
+        FormaIngresso obj = formaIngressoRepository.findById(id);
+        if(obj != null){
+            obj.setNome(formaIngresso.getNome());
+            formaIngressoRepository.persist(obj);
+        }
+    }
+
+    public void RemoverFormaIngressoService(Long id){
+        formaIngressoRepository.deleteById(id);
     }
 }
